eb4d90efdfda55366b23371f9a01f9977267bbaa
hbehboudi <hbehboudi@ce.sharif.edu>
2021-05-17 03:12:13 +0430

    add NeuralNetwork

--- MIR_Phase2/src/classification/models/nn.py
+++ MIR_Phase2/src/classification/models/nn.py
@@ -1,23 +1,15 @@
-import typing as th
 from sklearn.base import BaseEstimator, ClassifierMixin
-
-# since you can use sklearn (or other libraries) implementations for this task,
-#   you can either initialize those implementations in the provided format or use them as you wish
 from sklearn.neural_network import MLPClassifier
 
 
 class NeuralNetwork(BaseEstimator, ClassifierMixin):
-    def __init__(
-            self,
-            # add required hyper-parameters (if any)
-    ):
-        # todo: initialize parameters
-        pass
+    def __init__(self, alpha):
+        self.clf = MLPClassifier(solver='lbfgs', alpha=alpha,
+                                 hidden_layer_sizes=(5, 2), random_state=1)
 
     def fit(self, x, y, **fit_params):
-        # todo: for you to implement
+        self.clf.fit(x, y)
         return self
 
     def predict(self, x):
-        # todo: for you to implement
-        pass
+        return self.clf.predict(x)
--- MIR_Phase2/src/classification/models/svm.py
+++ MIR_Phase2/src/classification/models/svm.py
@@ -1,17 +1,14 @@
+from sklearn import svm
 from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVR
 
 
 class SVM(BaseEstimator, ClassifierMixin):
     def __init__(self, c):
-        self.c = c
-        self.regr = make_pipeline(StandardScaler(), SVR(C=c, epsilon=0.2))
+        self.clf = svm.SVC(kernel='linear', C=c)
 
     def fit(self, x, y, **fit_params):
-        self.regr.fit(x, y)
+        self.clf.fit(x, y)
         return self
 
     def predict(self, x, y=None):
-        return self.regr.predict(x)
+        return self.clf.predict(x)
